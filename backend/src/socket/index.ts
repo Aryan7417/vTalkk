15928717fadff9202b41ba1b316976829eca546f
Aryan7417 <caryan830@gmail.com>
2026-08-19 02:01:14 +0530

    expo setup

--- backend/src/socket/index.ts
+++ backend/src/socket/index.ts
@@ -1,13 +1,25 @@
 import { Server as HttpServer } from "http";
 import { Server as SocketServer } from "socket.io";
+
 import { registerCallSocket } from "./call.socket";
+
+import {
+  socketAuthMiddleware,
+  AuthenticatedSocket,
+} from "./socket.middlewere";
 
 let io: SocketServer;
 
 // User ID -> Socket ID
+
+
 const onlineUsers = new Map<string, string>();
 
 export const initSocket = (server: HttpServer) => {
+
+
+  // Create Socket.io server
+
   io = new SocketServer(server, {
     cors: {
       origin: "*",
@@ -15,38 +27,51 @@ export const initSocket = (server: HttpServer) => {
     },
   });
 
-  io.on("connection", (socket) => {
+  //Authenticate every socket connection
+
+
+  io.use(socketAuthMiddleware);
+
+  // Handle socket connection
+
+
+  io.on("connection", (socket: AuthenticatedSocket) => {
     console.log(`🔌 User connected: ${socket.id}`);
 
-    // Register user
-    socket.on("register-user", (userId: string) => {
+    // User ID comes from JWT
+    const userId = socket.userId;
+
+    if (userId) {
+      // Save User ID -> Socket ID
       onlineUsers.set(userId, socket.id);
 
       console.log(
         `🟢 User online: ${userId} -> ${socket.id}`
       );
-    });
+    }
 
-    // User disconnect
+    //  User disconnect
     socket.on("disconnect", () => {
-      for (const [userId, socketId] of onlineUsers.entries()) {
-        if (socketId === socket.id) {
+      if (userId) {
+        const currentSocketId = onlineUsers.get(userId);
+
+        // Only delete if this is the user's current socket
+        if (currentSocketId === socket.id) {
           onlineUsers.delete(userId);
-
-          console.log(
-            `🔴 User offline: ${userId}`
-          );
-
-          break;
         }
+
+        console.log(
+          `🔴 User offline: ${userId}`
+        );
       }
 
       console.log(
-        `❌ User disconnected: ${socket.id}`
+        `❌ Socket disconnected: ${socket.id}`
       );
     });
   });
 
+  // 📞 Register call events
   registerCallSocket(io, onlineUsers);
 
   console.log("⚡ Socket.io server initialized");
@@ -54,14 +79,20 @@ export const initSocket = (server: HttpServer) => {
   return io;
 };
 
+
+// Get Socket.io instance
 export const getIO = () => {
   if (!io) {
-    throw new Error("Socket.io is not initialized");
+    throw new Error(
+      "Socket.io is not initialized"
+    );
   }
 
   return io;
 };
 
+
+// Get online users
 export const getOnlineUsers = () => {
   return onlineUsers;
 };
