23fba11b0f389130d59fff4698b9c15fa7120174
Aryan7417 <caryan830@gmail.com>
2026-08-19 00:51:01 +0530

    complete authentication

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -2,11 +2,10 @@ import { Router } from "express";
 
 import {
   updateProfileController,
+  getAllUsersController,
 } from "../controller/user.controller";
 
-import {
-  authMiddleware,
-} from "../middleware/auth.middlewere";
+import { authMiddleware } from "../middleware/auth.middlewere";
 
 const router = Router();
 
@@ -14,6 +13,12 @@ router.patch(
   "/profile",
   authMiddleware,
   updateProfileController
+);
+
+router.get(
+  "/",
+  authMiddleware,
+  getAllUsersController
 );
 
 export default router;
